41259a2e99aa78f2613e4f11853679692033ea1b
wakenedo <a_nuner@outlook.com>
2023-06-27 09:44:41 -0300

    Refactor

--- desafio-nos/src/App.tsx
+++ desafio-nos/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import Tree, { TreeNodeProps } from 'react-d3-tree';
+import Tree from 'react-d3-tree';
 
 interface Node {
   name: string;
@@ -63,8 +63,7 @@ const App: React.FC = () => {
     setData({ ...data });
   };
 
-
-  const renderCustomNode = ({ nodeDatum }: TreeNodeProps) => {
+  const renderCustomNode = ({ nodeDatum }: any) => {
     if (nodeDatum.name === 'Else') {
       return (
         <foreignObject width="100" height="100" x="-50" y="-50">
@@ -129,80 +128,16 @@ const App: React.FC = () => {
     );
   };
 
-  const renderTreeNode = (node: Node) => {
-    return (
-      <div>
-        {node.name !== 'Start' && ( // Only render buttons for generated nodes
-          <React.Fragment>
-            <button
-              onClick={() => addNewNodeOnSameLevel(node.name, false)}
-              style={{ backgroundColor: 'green', marginRight: '10px' }}
-            >
-              Add Node to {node.name}
-            </button>
-          </React.Fragment>
-        )}
-        {node.children && node.children.length > 0 && (
-          <div style={{ marginLeft: '20px' }}>
-            {node.children.map((childNode) => renderTree([childNode]))}
-          </div>
-        )}
-        {!node.children && node.name !== 'Start' && (
-          <div style={{ marginLeft: '20px' }}>
-            <button
-              onClick={() => addNewNodeOnSameLevel(node.name, false)}
-              style={{ backgroundColor: 'green', marginRight: '10px' }}
-            >
-              Add Node to {node.name}
-            </button>
-            <button
-              onClick={() => addNewNodeOnSameLevel(node.name, true)}
-              style={{ backgroundColor: 'yellow', marginRight: '10px' }}
-            >
-              Add Node++ to {node.name}'s branch
-            </button>
-          </div>
-        )}
-        {!node.children && node.name === 'Start' && (
-          <div style={{ marginLeft: '20px' }}>
-            <button
-              onClick={addNewNodeAtRootLevel}
-              style={{ backgroundColor: 'yellow', marginRight: '10px' }}
-            >
-              Add Node
-            </button>
-            <button
-              onClick={() => addNewNodeOnSameLevel(node.name, true)}
-              style={{ backgroundColor: 'yellow', marginRight: '10px' }}
-            >
-              Add Node++ to {node.name}'s branch
-            </button>
-          </div>
-        )}
-      </div>
-    );
-  };
-
-
-  const renderTree = (nodes: Node[]) => {
-    return nodes.map((node: Node) => (
-      <React.Fragment key={`node-${node.name}`}>
-        {renderTreeNode(node)}
-      </React.Fragment>
-    ));
-  };
-
   return (
     <div style={{ width: '100%', height: '100vh' }}>
       <Tree
         data={data}
         orientation="horizontal"
-        collapsible={true}
+        collapsible={true} // Enable collapsible feature
         translate={{ x: 300, y: 100 }}
         nodeSize={{ x: 200, y: 100 }}
         renderCustomNodeElement={renderCustomNode}
       />
-      <div>{renderTree(data.children || [])}</div>
     </div>
   );
 };
